0b467b196cbf444600d800f9f0d0d0e3e98fd983
frodochen <frodochen@tencent.com>
2018-10-26 17:24:53 +0800

    split tasks to flashcard limit count

--- src/pkg/r2d2/word/Main.java
+++ src/pkg/r2d2/word/Main.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public class Main {
 
+    private static final int EVERY_FILE_WORD_COUNT_LIMIT = 300;
+
     public static void main(String[] args) {
         // write your code here
         ILookup youdaoLookup = new SimpleYoudaoLookup();
@@ -17,15 +19,33 @@ public class Main {
             List<String> lines = FileUtils.readLines(new File("in.txt"), "utf-8");
             List<String> outLines = new ArrayList<>();
             int count = 0;
+            int wordCount = 0;
+            int fileIndex = 0;
             for (String line : lines) {
                 if (line.trim().length() >= 3) {
                     count++;
-                    System.out.println("look up:  " + line.trim());
+                    wordCount++;
+                    System.out.println(" look up:  " + line.trim());
                     outLines.add(youdaoLookup.doLookup(line.trim()).toString());
+
+                    if (wordCount >= EVERY_FILE_WORD_COUNT_LIMIT) {
+                        ++fileIndex;
+                        FileUtils.writeLines(new File("wordOut", "out_" + fileIndex + ".txt"), outLines);
+                        outLines.clear();
+                        wordCount = 0;
+
+                        System.out.println(">>>>>>>> write result to file: out_" + fileIndex);
+                    }
                 }
             }
-            FileUtils.writeLines(new File("out.txt"), outLines);
-            System.out.println("look up finished: count: " + count);
+
+            if (!outLines.isEmpty()) {
+                ++fileIndex;
+                FileUtils.writeLines(new File("wordOut", "out_" + fileIndex + ".txt"), outLines);
+
+                System.out.println(">>>>>>>> write result to file: out_" + fileIndex);
+            }
+            System.out.println(" ******* look up finished: count: " + count);
         } catch (IOException e) {
             e.printStackTrace();
         }
